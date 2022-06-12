b16b4d91ee4648e670dd1a8992be3db70f8c76e0
Oscar Reyes <oscarreyes@Oscars-MBP.home>
2022-06-12 17:51:51 +0200

    Adding schema design and crud

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-const express = require('express');
-const app = express();
-const dotenv = require('dotenv');
-const cors = require('cors');
-const PORT = 3001;
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
+const projectRoutes = require('./routes/projectRoutes')
+const bodyParser = require('body-parser')
 
-dotenv.config; // load .env file
+const app = express()
+const PORT = 3001
 
-app.use(cors()); // enable cors
+//!--> Connection String to MongoDB user: netflix / password: netflix / db name: proj-mng <--
+const dbURI =
+  'mongodb+srv://netflix:netflix@cluster0.wqxvqva.mongodb.net/proj-mng?retryWrites=true&w=majority'
+
+//* Mongoose module to connect to our database:
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(function (result) {
+    console.log('Database is connected')
+  })
+  .catch((err) => console.log(err))
+
+//! const dotenv = require('dotenv')
+//! dotenv.config
+
+//* enable cors
+app.use(cors())
+
+app.get('/', (request, response) => response.send('Hello from the other side!'))
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`); // print to console when server is running 
-});
+  console.log(`Server is listening on port: http://localhost:${PORT}`) // print to console when server is running
+})
 
+/**
+ * ! Parses the text as JSON and exposes teh resulting obj on req.body
+ * * bodyParser.json(options)
+ * */
 
+app.use(bodyParser.json())
+app.use('projects', projectRoutes)
