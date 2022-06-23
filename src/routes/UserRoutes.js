d31f5835c2499a4bb45b1e3a87e3510da2be96b5
Arian <arian.collaso.rodriguez@gmail.com>
2022-06-23 14:37:54 +0200

    Creo y pruebo la ruta de Users con jwt, compruebo que funcionan todos los metdodos

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,71 +1,69 @@
 const express = require('express');
 const User = require('../mongo/schemas/user');
+const verifyToken = require('../helpers/verifyToken')
+
 
 const UserRouter = express.Router();
 
- // GET all users
-UserRouter.get('/', async(request, response) => {
+// GET all users
+UserRouter.get('/', async (request, response) => {
     const users = await User.find({})
     try {
         response.send(users)
-        } catch (error) {
-            response.status(500).json(error)
-        }
-})
-
-
-
-// GET one user
-UserRouter.get('/:id', async(request, response) => {
-    const id = request.params.id
-    try {
-        const user = await User.findById({_id: id});
-        if(!user) response.status(404).json("No user found")
-        return response.status(200).json(user)
     } catch (error) {
-        console.log(error)
         response.status(500).json(error)
     }
 })
 
+// - GET ONE USER
+UserRouter.get('/:id', async (request, response) => {
+    const id = request.params.id
+    try {
+        const user = await User.findById(id)
+        if (!user) return response.status(404).json({ message: 'No user found' })
+        if (request.auth.id !== request.params.id) {
+            return response.status(403).json({ message: 'Not authorized' })
+        } else {
+            return response.status(200).json(user)
+        }
+    } catch (error) {
+        response.status(500).json(error)
+    }
+})
 
 //  DELETE one user
-UserRouter.delete('/:id', async (request, response)=>{
-    try{
-         // find user by id and then delete user
-        const user = await User.findByIdAndDelete(request.params.id)
-         // if user doesn't exist, then error 404 with message "User not found" if exist then delete user
-        if(!user){
-            return response.status(404).json("No user found")
-        }else{
+UserRouter.delete('/:id', async (request, response) => {
+    try {
+
+        const user = await User.findById(request.params.id)
+        if (!user) response.status(404).json({ message: 'No user found' })
+        if (request.auth.id !== request.params.id) {
+            return response.status(403).json({ message: 'Not authorized' })
+        } else {
+            await User.findByIdAndDelete(request.params.id);
             return response.status(204).json("User deleted")
-            console.log(`User ${user.name} deleted`)
         }
-        console.log(user)
     }
-     // return error 500 if there is an error with the server-side
-    catch(err){
-        return response.status(500).json({error: err.message})
+    catch (err) {
+        return response.status(500).json({ error: err.message })
     }
 })
 
 
-
-// PATCH 
-UserRouter.patch('/:id', async(request, response)=>{
-    try{
-        // find user by id and then update user data
-        const userUpdate = await User.findByIdAndUpdate({_id : request.params.id }, request.body, {new: true})
-        // if user doesn't exist, then error 404 with message "User not found"
-        if(!user) response.status(404).json({error: "User not found"})
-        // if request auth doesn't match with any user id, then return error 403 with message "Not allowed"
-        if(request.auth.id !== user.id) response.status(403).json({error: "Not allowed"}) ;
-        // if user exists, return user data updated
-        return response.status(200).json(userUpdate)
-    }
-        // return error 500 if there is an error with the server-side
-    catch(err){
-        response.status(500).json({error: err.message})
+UserRouter.patch('/:id', async (request, response) => {
+    try {
+        const user = await User.findById(request.params.id)
+        if (!user) response.status(404).json({ message: 'User not found' })
+        if (request.auth.id !== request.params.id) {
+            return response.status(403).json({ message: 'Not authorized' })
+        } else {
+            await User.findByIdAndUpdate({ _id: request.params.id }, request.body, { new: true })
+            return response.status(200).json({ message: 'Content updated' })
+            
+        }
+        
+    } catch (err) {
+        return response.status(500).json({ message: err.message })
     }
 })
 
