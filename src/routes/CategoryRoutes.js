28be4aa41576caa5144c7639b28ec0194d7642f6
insert-oreyes <oreyes.s@outlook.com>
2022-07-29 19:16:48 +0200

    categories finished

--- src/routes/CategoryRoutes.js
+++ src/routes/CategoryRoutes.js
@@ -9,6 +9,7 @@ const {
 
 const CategoriesRouter = express.Router()
 
+//· POST!
 CategoriesRouter.post('/', isAuthorized, async (request, response) => {
   const newCategory = new Category(request.body)
   try {
@@ -19,8 +20,46 @@ CategoriesRouter.post('/', isAuthorized, async (request, response) => {
   }
 })
 
+// · GET by /:id!
+CategoriesRouter.get('/:id', async (request, response) => {
+  const id = request.params.id
+  try {
+    const category = await Category.findById(id)
+    if (!category)
+      return response.status(404).json({ message: 'No category found' })
+    response.status(200).json(category)
+  } catch (error) {
+    response.status(500).json(error)
+  }
+})
 
+// · DELETE!
+CategoriesRouter.delete('/:id', isAuthorized, async (request, response) => {
+  try {
+    const category = await Category.findByIdAndDelete(request.params.id)
 
-CategoriesRouter.patch('/:id', isAuthorized, async (request, response) => {})
+    if (!category) response.status(404).send('No item found')
+    response.status(200).json()
+  } catch (error) {
+    response.status(500).json(error)
+  }
+})
+
+// · PATCH!
+CategoriesRouter.patch('/:id', async (request, response) => {
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true }
+    )
+    if (!updatedCategory) {
+      response.status(404).send('No item found')
+    }
+    response.status(200).json(updatedCategory)
+  } catch (error) {
+    response.status(500).json(error)
+  }
+})
 
 module.exports = CategoriesRouter
